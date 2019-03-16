f9eac3fd06fb65e2967c2546d1cd35dc0cd36236
Marty <45027048+mart@users.noreply.github.com>
2019-03-16 15:15:28 -0700

    progress

--- cafes.py
+++ cafes.py
@@ -15,6 +15,7 @@ class Cafe:
         self.num_ratings = ratings
         self.busy = busy
         self.id = id
+        self.link = "https://www.google.com/maps/place/?q=place_id:" + id
 
 
 # Returns a list of dictionaries, containing cafes 1km around lat, long
@@ -24,7 +25,7 @@ def get_locations(lat, lon):
     max_lat = lat + dist
     min_lon = lon - dist
     max_lon = lon + dist
-    return populartimes.get(environ["APIKEY"], ["cafe"], (min_lat, min_lon), (max_lat, max_lon), 20, 1000)
+    return populartimes.get(environ["APIKEY"], ["cafe"], (min_lat, min_lon), (max_lat, max_lon), 20, 50)
 
 
 def sort_cafes(locs, busy_pref, rating_pref):
@@ -36,7 +37,7 @@ def sort_cafes(locs, busy_pref, rating_pref):
             popularity = cafe["populartimes"][weekdate]["data"][cur_hour]
 
         score = round((100 - busy_pref*popularity)/2 + rating_pref*cafe["rating"]*10, 2)
-        sorted_cafes.append(Cafe(cafe["name"], cafe["address"], cafe["rating"], score,
+        sorted_cafes.append(Cafe(cafe["name"], cafe["address"], cafe["rating"], int(score),
                                  cafe["rating_n"], popularity, cafe["id"]))
     sorted_cafes = sorted(sorted_cafes, key=lambda x: x.score, reverse=True)
     return sorted_cafes
